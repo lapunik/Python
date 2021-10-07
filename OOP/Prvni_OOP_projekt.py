5b91778c5fb75c053e05e2bd0dd52e6d324c8edf
Lapi <vojtechlapunik@gmail.com>
2021-10-07 15:33:04 +0200

    Posledni zbytecny testovaci commit, od ted jedeme na ostro

--- OOP/Prvni_OOP_projekt.py
+++ OOP/Prvni_OOP_projekt.py
@@ -1,9 +1,1 @@
 # změnil jsem projekt 
-<<<<<<< HEAD
-
-# Tady dám jiný komentář, aby nastal konflikt mezi větvemi
-
-# dalsi zmena
-=======
-# je potřeba tady něco změnit prostě no
->>>>>>> pokus_s_novou_vetvi
